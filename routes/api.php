3c95d37e8bb6b7253a0e923f4ee256801a96f381
Bozic01 <aobozic@gmail.com>
2023-09-09 19:08:31 +0200

    Update AuthController.php, ProductController.php, and 3 more files...

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@ Route::middleware(['auth:sanctum','admin'])->group(function () {
     Route::get('/user',function(Request $request){
         return $request->user();
     });
-    Route::post('/logout',[\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('/logout',[\App\Http\Controllers\Api\AuthController::class, 'logout']);
 
-    Route::apiResource('/product',\App\Http\Controllers\ProductController::class);
+    Route::apiResource('/product',\App\Http\Controllers\Api\ProductController::class);
 });
 
 Route::post('/login',[\App\Http\Controllers\AuthController::class, 'login']);
