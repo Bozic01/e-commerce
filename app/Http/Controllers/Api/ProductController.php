cfb16abb8cc51fcd4b9e2ca3554cfe78b2f7fbf9
Bozic01 <aobozic@gmail.com>
2023-09-09 19:08:23 +0200

    Update AuthController.php, ProductController.php, and 2 more files...

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProductListResource;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use App\Http\Requests\ProductRequest;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return ProductListResource::collection(Product::query()->paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ProductRequest $request)
+    {
+        $data = $request->validated();
+        $data['created_by'] = $request->user()->id;
+        $data['updated_by'] = $request->user()->id;
+
+        /** @var \Illuminate\Http\UploadedFile $image */
+
+        $image = $data['image'] ?? null;
+
+        if($image){
+            $relativePath = $this->saveImage($image);
+            $data['image'] = URL::to(Storage::url($relativePath));
+            $data['image_mime'] = $image->getClientMimeType();
+            $data['image_size'] = $image->getSize();
+        }
+
+        $product = Product::create($data);
+
+        return new ProductResource($product);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ProductRequest $request, Product $product)
+    {
+       $data = $request->validated();
+       $data['updated_by'] = $request->user()->id;
+
+       /** @var \Illuminate\Http\UploadedFile $image */
+        $image = $data['image'] ?? null;
+
+        if($image){
+            $relativePath = $this->saveImage($image);
+            $data['image'] = URL::to(Storage::url($relativePath));
+            $data['image_mime'] = $image->getClientMimeType();
+            $data['image_size'] = $image->getSize();
+
+            if($product->image){
+                Storage::deleteDirectory('/public/' . dirname($product->image));
+            }
+        }
+        $product->update($data);
+
+        return new ProductResource($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->noContent();
+    }
+
+    private function saveImage(\Illuminate\Http\UploadedFile $image)
+    {
+        $path = 'images/' . Str::random();
+        if(!Storage::exists($path)) {
+            Storage::makeDirectory($path, 0755, true);
+        }
+        if(!Storage::putFileAs('public/' . $path, $image, $image->getClientOriginalName())) {
+            throw new \Exception("Unable to save file \"{$image->getClientOriginalName()}\"");
+        }
+
+        return $path . '/' . $image->getClientOriginalName();
+    }
+}
