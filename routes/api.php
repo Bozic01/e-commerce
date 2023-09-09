0e02e8f405a2de48d4a7e02d397279588fc8ebaa
Bozic01 <aobozic@gmail.com>
2023-09-09 15:53:22 +0200

    small fix on route admin

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum','admin')->group(function(){
+Route::middleware(['auth:sanctum','admin'])->group(function () {
     Route::get('/user',function(Request $request){
         return $request->user();
     });
