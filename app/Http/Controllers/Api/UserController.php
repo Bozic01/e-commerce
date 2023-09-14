a5d5620c297561ec08ce9fa3daafe1660f73b52e
Bozic01 <aobozic@gmail.com>
2023-09-14 16:30:17 +0200

    logic for user management on admin side

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\Api\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $perPage = request('per_page',10);
+        $search = request('search','');
+        $sortField = request('sort_field','updated_at');
+        $sortDirection = request('sort_direction');
+
+        $query = User::query()
+           ->orderBy($sortField, $sortDirection)
+           ->paginate($perPage);
+
+        return UserResource::collection($query);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ProductRequest $request)
+    {
+        $data = $request->validated();
+        $data['is_admin'] = true;
+        $data['email_verified_at'] = date('Y-m-d H:i:s');
+        $data['password'] = Hash::make($data['password']);
+
+        $data['created_by'] = $request->user()->id;
+        $data['updated_by'] = $request->user()->id;
+
+        $user = User::create($data);
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ProductRequest $request, Product $product)
+    {
+       $data = $request->validated();
+      
+        if(!empty($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+        $data['updated_by'] = $request->user()->id;
+
+        $user->update($data);
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response()->noContent();
+    }
+
+   
+}
