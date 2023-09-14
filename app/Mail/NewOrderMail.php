4f7fbaddd68cbc76f30344d553f167a2a12659c9
Bozic01 <aobozic@gmail.com>
2023-09-14 15:25:48 +0200

    implement email for new order and updated order

---
+++ app/Mail/NewOrderMail.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Order;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NewOrderMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(public Order $order, public $forAdmin = true)
+    {
+        //
+    }
+
+
+    public function build()
+    {
+        return $this
+            ->subject('New Order')
+            ->view('mail.new-order');
+    }
+}
